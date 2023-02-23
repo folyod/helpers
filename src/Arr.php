cf04157d38179609be94ba707bb693443f246293
Ilya <ilya-sorokin91@yandex.ru>
2023-02-23 23:07:56 +0300

    update

--- src/Arr.php
+++ src/Arr.php
@@ -8,16 +8,29 @@ use Folyod\Helpers\Services\InDeepFinder;
 
 final class Arr
 {
+    /**
+     * @param array<mixed> $source
+     * @param non-empty-string $key
+     */
     public static function get(array &$source, string $key, mixed $default = null): mixed
     {
         return $source[$key] ?? $default;
     }
 
+    /**
+     * @param array<mixed> $source
+     * @param non-empty-string $key
+     */
     public static function has(array &$source, string $key): bool
     {
         return isset($source[$key]);
     }
 
+    /**
+     * @param array<mixed> $source
+     * @param non-empty-string $key
+     * @param non-empty-string $separator
+     */
     public static function getDeep(array &$source, string $key, mixed $default = null, string $separator = '.'): mixed
     {
         return InDeepFinder::find($key, $source, $separator) ?? $default;
--- src/Services/InDeepFinder.php
+++ src/Services/InDeepFinder.php
@@ -6,13 +6,22 @@ namespace Folyod\Helpers\Services;
 
 final class InDeepFinder
 {
-    public static function find(string $key, array &$source, string $separator): mixed
+    /**
+     * @param array<mixed> $source
+     * @param non-empty-string $key
+     * @param non-empty-string $separator
+     */
+    public static function find(string $key, array &$source, string $separator = '.'): mixed
     {
         $paths = explode($separator, $key);
 
         return self::findForKeys($paths, $source);
     }
 
+    /**
+     * @param array<mixed> $source
+     * @param array<non-empty-string> $keys
+     */
     private static function findForKeys(array &$keys, array &$source): mixed
     {
         $key = array_shift($keys);
